7aaa9e08dc3ea6728eaba3bf405109e475430e58
Jan Hernandez <jhernandez@bluerunnersolutions.com>
2018-02-04 21:14:17 +0000

    added serverless handler

--- handler.js
+++ handler.js
@@ -1,30 +1,28 @@
-const { graphqlLambda, graphiqlLambda } = require('apollo-server-lambda');
-const lambdaPlayground = require('graphql-playground-middleware-lambda');
+const server = require('apollo-server-lambda');
+const lambdaPlayground = require('graphql-playground-middleware-lambda').default;
 
-const { makeExecutableSchema } = require('graphql-tools');
 const { schema } = require('./schema');
 
-const myGraphQLSchema = makeExecutableSchema({
-  typeDefs: schema,
-  logger: console,
-});
+exports.graphqlHandler = server.graphqlLambda({ schema });
 
-exports.graphqlHandler = function graphqlHandler(event, context, callback) {
+/* exports.graphqlHandler = function graphqlHandler(event, context, callback) {
+  console.log(event);
   function callbackFilter(error, output) {
     // eslint-disable-next-line no-param-reassign
-    output.headers['Access-Control-Allow-Origin'] = '*';
+    // output.headers['Access-Control-Allow-Origin'] = '*';
+    console.log(error);
     callback(error, output);
   }
 
-  const handler = graphqlLambda({ schema: myGraphQLSchema, tracing: true });
+  const handler = graphqlLambda({ schema, tracing: true });
   return handler(event, context, callbackFilter);
-};
+}; */
 
 // for local endpointURL is /graphql and for prod it is /stage/graphql
 exports.playgroundHandler = lambdaPlayground({
   endpoint: '/graphql',
 });
 
-exports.graphiqlHandler = graphiqlLambda({
+exports.graphiqlHandler = server.graphiqlLambda({
   endpointURL: '/graphql',
 });
