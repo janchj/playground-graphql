efbc363028b3d48d1f373d3e7685a8157bc70fc4
Jan Hernandez <jhernandez@bluerunnersolutions.com>
2018-02-04 21:14:30 +0000

    linting changes

--- mongodb/load-test-data.js
+++ mongodb/load-test-data.js
@@ -2,19 +2,19 @@ const { MongoClient } = require('mongodb');
 const assert = require('assert');
 
 MongoClient.connect('mongodb://localhost:27017/cocktails', (err, client) => {
-    assert.equal(null, err);
-    var db = client.db('cocktails');
-    db.collection('recipes').insertMany([
-        {
-            recipeId: 0,
-            spiritId: 1
-        },
-        {
-            recipeId: 1,
-            spiritId: 2
-        }
-    ]).then(response => {
-        console.log(response);
-        client.close();
-    });
+  assert.equal(null, err);
+  const db = client.db('cocktails');
+  db.collection('recipes').insertMany([
+    {
+      recipeId: 0,
+      spiritId: 1
+    },
+    {
+      recipeId: 1,
+      spiritId: 2
+    }
+  ]).then((response) => {
+    console.log(response);
+    client.close();
+  });
 });
